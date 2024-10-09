40a0b31e5b80ba411e8dbe41df6833546bfa8420
ShadDenis <anastasiabelova68206@gmail.com>
2024-10-09 20:41:46 +0700

    09.10.2024
    итог

---
+++ lala.py
@@ -0,0 +1,41 @@
+# import calendar
+# string = input ("Введите число дня")
+# print(list(calendar.day_name)[int(string)])
+# from mumu.krya import string
+
+# import re
+# string = input("ведите число")
+# print(re.sub(r'[^.\d]', "",string))
+# if string.isdigit() or string.find(".") > 0:
+#     if int(string) > 0:
+#         print("Положительно")
+#     if int(string) < 0:
+#         print("отрицательно")
+#     else:
+#         print("число = 0")
+# else:
+#     print("Вы ввели не число")
+#
+
+# a = input ().split(" ")
+# if int (a[0]) == int(a[1]):
+#     print("Числа равные", a[0])
+# else:
+#     if int (a[0]) > int(a[1]):
+#         print("2 число:", int(a[1]))
+#         print("1 число:", int(a[0]))
+#     else:
+#         print("1 число:", int(a[0]))
+#         print("2 число:", int(a[1]))
+
+
+
+
+string = (input().split(" ")
+for i in string:
+    print(i)
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+string = (input().split(" "))
+numbers = []
+for i in string:
+    numbers.append(int(i))
+    numbers.sort()
+print(numbers)
+#пузырьковая сортировка
+
