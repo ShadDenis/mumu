3ac0ec6f17fb132f24548d794040bedf7c8681e3
ShadDenis <anastasiabelova68206@gmail.com>
2024-10-23 20:48:59 +0700

    23.10.24
    23.10.24

--- test10.py
+++ test10.py
@@ -31,10 +31,12 @@ for i in range(int(string[0]), int(string[1])):
         if i % j == 0:
             count += 1
         j += 1
+        if count > 2:
+            break
     if count == 2:
         numbers.append(i)
 end_time=datetime.today()
 delta = end_time - start_time
-print("простые числа:",numbers , "")
+print("простые числа:",numbers , "\n", delta)
 
 
