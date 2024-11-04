eeef4e116ce1ca8c6ad1b07e357534cbed86a847
ShadDenis <anastasiabelova68206@gmail.com>
2024-11-04 20:08:22 +0700

    04.11.24
    04.11.24

--- doptest.py
+++ doptest.py
@@ -1,12 +1,12 @@
 #найти наибольшее число в массиве, являющиеся полным квадратом некоторого числа
 
 
-num = []
-s = input().split(" ")
-max_square = 0
-for i in range(int(s[0]), int(s[1])):
-    sqrt = int(abs(i) ** 0.5)
-    # print(sqrt)
-    if i == sqrt ** sqrt:
-        num.append(i)
-print(num)
+# num = []
+# s = input().split(" ")
+# max_square = 0
+# for i in range(int(s[0]), int(s[1])):
+#     sqrt = int(abs(i) ** 0.5)
+#     # print(sqrt)
+#     if i == sqrt ** sqrt:
+#         num.append(i)
+# print(num)
---
+++ test15.py
@@ -0,0 +1,59 @@
+
+
+# x = 5
+# y = 5
+# print(x is y)# is это ==
+
+
+
+#треугольнички
+#ж
+# print(" " * 20, "_" * 43 )
+# i = 1
+# while i < 40:
+#     print(" "*20 + "|" ,
+#     "*" * i if i <=20 else "*" * (40 - i),
+#     " " * (40-i) if i<= 20 else " " * i ,
+#     "|")
+#     i+=1
+# print(" " * 20, "-" * 43 )
+
+
+#е
+# print(" " * 29, "_" * 45 )
+# i = 1
+# while i < 40:
+#     if i <= 20:
+#         print(" " * 20 + "|",
+#         "*" * i, " " *(40 - i * 2), "*" * i , "|" )
+#     else:
+#         print(" " * 20 + "|",
+#              "*" * (40-i),
+#              " " * ((i - 20) * 2),
+#              "*" * (40-i), "|")
+#     i+=1
+#
+#     print(" " * 20, "-" * 43 )
+    #не успел((((
+
+
+
+
+#13
+#№2
+spisok = [-1, -2, -4, -5, 0, 3, 3, 5, 6, 63, 3, 4]
+positive = 0
+negative = 0
+nulls = 0
+for x in spisok:
+    if x < 0:
+        negative+=1
+    if x > 0:
+        positive+=1
+    if x == 0:
+        nulls+=1
+
+
+print(max(spisok))
+print(min(spisok))
+print(positive, negative, nulls)
